f9f69c0771f6e2b7686b7323d309969e1307043c
kanaklatwal <kanaklatwal@gmail.com>
2026-05-18 17:18:54 +0530

    feat: add login functionality and protect routes with JWT authentication

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -49,3 +49,50 @@ message:"Server Error"
 }
 
 };
+
+export const login = async (
+ req: Request,
+ res: Response
+) => {
+
+try{
+
+const {email,password}=req.body;
+
+const user=await User.findOne({email});
+
+if(
+user &&
+await bcrypt.compare(
+password,
+user.password
+)
+){
+
+return res.json({
+
+_id:user._id,
+name:user.name,
+email:user.email,
+role:user.role,
+token:generateToken(
+user._id.toString()
+)
+
+});
+
+}
+
+return res.status(401).json({
+message:"Invalid credentials"
+});
+
+}catch(error){
+
+return res.status(500).json({
+message:"Server Error"
+});
+
+}
+
+};
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,11 +1,17 @@
 import express from "express";
-import { register } from "../controllers/auth.controller";
+import { register, login } from "../controllers/auth.controller";
+import { protect } from "../middleware/auth.middleware";
 
 const router=express.Router();
 
-router.post(
-"/register",
-register
-);
+router.post("/register", register);
+router.post("/login", login);
 
+router.get("/me", protect, (req, res) => {
+    res.json({
+      message: "Authorized user",
+      userId: req.body.userId
+    });
+  });
+  
 export default router;
